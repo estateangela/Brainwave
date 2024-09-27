83e8096b24b494bb3e36518d69c2fb1471423d8d
e.an <161005688+estateangela@users.noreply.github.com>
2024-09-27 15:55:43 +0800

    import index and create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import ButtonGradient from './assets/svg/ButtonGradient'; // 确保路径正确
-import Button from './components/Button';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world
-      </h1>
-      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
-        <Button className='mt-20' href='#login'>
-          something
-        </Button>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] 
+      overflow-hidden'>
+        <Header/>
       </div>
       <ButtonGradient />
       </>
